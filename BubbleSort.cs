c3fc578a29dc33270af35c9ce22c3c198d96015c
subodhNagalwade <subodhnagalwade@gmail.com>
2022-09-21 15:34:15 +0530

    BubbleSort

---
+++ BubbleSort.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoProblem16
+{
+    internal class BubbleSort
+    {
+        public static void Bubblesort()
+        {
+            int[] arr = { 76, 35, 45, 38, 53 };
+            int temp;
+            for (int j = 0; j <= arr.Length - 2; j++)
+            {
+                for (int i = 0; i <= arr.Length - 2; i++)
+                {
+                    if (arr[i] > arr[i + 1])
+                    {
+                        temp = arr[i + 1];
+                        arr[i + 1] = arr[i];
+                        arr[i] = temp;
+                        Console.WriteLine("Sorted:");
+                        foreach (int p in arr)
+                            Console.WriteLine(p + " ");
+                        Console.ReadKey();
+                    }
+                }
+            }
+        }
+    }
+}
